ec255a73250058ba94f2007b701624090212bf1e
Thomas Andreo <thomas.andreo@gmail.com>
2015-12-03 20:37:18 -0500

    Moved random generation logic to new class

---
+++ app/Services/RandomStuffGenerator.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Services;
+
+class RandomStuffGenerator
+{
+    public function getImageUrl ()
+    {
+        $folder = 'cats';
+
+        $path = base_path().'/public/img/'.$folder.'/';
+        $files = array_values(array_diff(scandir($path), array('..', '.','.DS_Store')));
+        $fileName = $files[rand(0,count($files)-1)];
+
+        $fileUrl = url('img').'/'.$folder.'/'.$fileName;
+
+        return $fileUrl;
+    }
+}
